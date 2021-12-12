b3d4ee0c20efecb22e7be5e3d7cb645f232abfa1
NatiPeralta <nati.g.peralta@hotmail.com>
2021-12-11 23:30:01 -0300

    Criando tela de cadastro e login

---
+++ app/src/main/java/br/com/filnat/finalworkn2androidstudio/activity/CadastroActivity.java
@@ -0,0 +1,16 @@
+package br.com.filnat.finalworkn2androidstudio.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.filnat.finalworkn2androidstudio.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/br/com/filnat/finalworkn2androidstudio/activity/LoginActivity.java
+++ app/src/main/java/br/com/filnat/finalworkn2androidstudio/activity/LoginActivity.java
@@ -1,14 +1,14 @@
-package br.com.filnat.finalworkn2androidstudio;
+package br.com.filnat.finalworkn2androidstudio.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
