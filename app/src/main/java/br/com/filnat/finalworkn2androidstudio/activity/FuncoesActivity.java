d02d2e19e0a38d4498a59952c9814e0454b19fa6
FDAPADILHA <filipe.amaral.padilha@gmail.com>
2021-12-13 01:49:39 -0300

    adicionando funcao ao botao buttonAcessar e buttonCadastre; criacao da nova activity layout activity_funcoes para as funcoes do aplicativo e da activity FuncoesActivity

---
+++ app/src/main/java/br/com/filnat/finalworkn2androidstudio/activity/FuncoesActivity.java
@@ -0,0 +1,25 @@
+package br.com.filnat.finalworkn2androidstudio.activity;
+
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import br.com.filnat.finalworkn2androidstudio.R;
+
+public class FuncoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_funcoes);
+
+        initView();
+
+    }
+
+    private void initView() {
+
+    }
+
+}
--- app/src/main/java/br/com/filnat/finalworkn2androidstudio/activity/LoginActivity.java
+++ app/src/main/java/br/com/filnat/finalworkn2androidstudio/activity/LoginActivity.java
@@ -6,16 +6,56 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import br.com.filnat.finalworkn2androidstudio.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button buttonCadastre;
+    private Button buttonAcessar;
+    private EditText editTextLogin, editTextSenhaLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initView();
+        setButtonCadastre();
+        setButtonAcessar();
     }
 
+    public void initView() {
 
+        editTextLogin = findViewById(R.id.editTextLogin);
+        editTextSenhaLogin = findViewById(R.id.editTextSenhaLogin);
+        buttonCadastre = findViewById(R.id.buttonCadastre);
+        buttonAcessar = findViewById(R.id.buttonAcessar);
+
+    }
+
+    public void setButtonCadastre() {
+
+        buttonCadastre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    public void setButtonAcessar() {
+
+        buttonAcessar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, FuncoesActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
 }
