13e2124de2848f45b7cf1f2ae9948614dce4a0d5
NatiPeralta <nati.g.peralta@hotmail.com>
2021-12-12 22:39:46 -0300

    Criando a tela de Login

--- app/src/main/java/br/com/filnat/finalworkn2androidstudio/activity/LoginActivity.java
+++ app/src/main/java/br/com/filnat/finalworkn2androidstudio/activity/LoginActivity.java
@@ -2,13 +2,24 @@ package br.com.filnat.finalworkn2androidstudio.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import br.com.filnat.finalworkn2androidstudio.R;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+
+    public void cadastrar(View v){
+        Intent i = new Intent(this,CadastroActivity.class);
+        startActivity(i);
     }
 }
